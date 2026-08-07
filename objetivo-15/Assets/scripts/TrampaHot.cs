44e485ccd658ae40e724e64ec00683e9006097a8
d4vid <sebastianchaparro6090@gmail.com>
2026-08-07 18:36:45 -0500

    trampassssss

---
+++ objetivo-15/Assets/scripts/TrampaHot.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class TrampaHot : MonoBehaviour
+{
+    [Header("TrampaHot timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activateTime;
+
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+    private bool triggered;
+    private bool active;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+        if (!triggered)
+            StartCoroutine(ActivateTrampaHot());
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+        if (!active) return;
+
+        MuerteJugador muerte = collision.GetComponent<MuerteJugador>();
+        if (muerte != null)
+            muerte.MorirPorEnemigo();
+    }
+
+    private IEnumerator ActivateTrampaHot()
+    {
+        triggered = true;
+        spriteRend.color = Color.red;
+        yield return new WaitForSeconds(activationDelay);
+        active = true;
+        yield return new WaitForSeconds(activateTime);
+        active = false;
+        spriteRend.color = Color.white;
+        triggered = false;
+    }
+}
